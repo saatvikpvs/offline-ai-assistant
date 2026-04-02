dfe6353d2be9ab5861ad309ce668874f29350b0b
saatvikpvs <pvssaatvik@gmail.com>
2026-04-02 22:18:39 +0530

    not npi3 it is phi3

--- config/llm_config.py
+++ config/llm_config.py
@@ -3,7 +3,7 @@ from crewai import LLM
 def get_llm():
 
     return LLM(
-        model="ollama/npi3",
+        model="ollama/phi3",
         base_url="http://localhost:11434",
         temperature=0.3,
         max_tokens=80   # limit response length
