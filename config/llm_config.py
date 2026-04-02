a381d36226be7e38f6b81dccb0452acca424da77
saatvikpvs <pvssaatvik@gmail.com>
2026-04-02 22:07:44 +0530

    npi3 change in config

--- config/llm_config.py
+++ config/llm_config.py
@@ -3,7 +3,7 @@ from crewai import LLM
 def get_llm():
 
     return LLM(
-        model="ollama/mistral",
+        model="ollama/npi3",
         base_url="http://localhost:11434",
         temperature=0.3,
         max_tokens=80   # limit response length
